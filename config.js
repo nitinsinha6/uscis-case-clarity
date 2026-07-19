1f6849b4dbd61382dd2c1a6bd3786c50e457fbdf
Nitin <155204489+nitinsinha6@users.noreply.github.com>
2026-07-18 18:01:19 -0700

    Update extension URL and contact email in config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
  * Use hosted payment links, such as Stripe Payment Links, Ko-fi, or Buy Me a Coffee.
  */
 window.SITE_CONFIG = {
-  extensionUrl: "https://chromewebstore.google.com/",
+  extensionUrl: "https://chromewebstore.google.com/detail/pmfhomnifobhdenknahmdaekhfjnhfkf/preview?hl=en-GB&authuser=0",
   donationUrl: "https://www.buymeacoffee.com/",
-  contactEmail: "support@example.com",
+  contactEmail: "nitsin.ca@gmail.com",
   productName: "Case Clarity"
 };
