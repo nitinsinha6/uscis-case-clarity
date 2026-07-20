8d74a436597b6571f1b807428639401b1c40654f
Nitin <155204489+nitinsinha6@users.noreply.github.com>
2026-07-19 17:14:45 -0700

    Update extension URL in configuration

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  * Use hosted payment links, such as Stripe Payment Links, Ko-fi, or Buy Me a Coffee.
  */
 window.SITE_CONFIG = {
-  extensionUrl: "https://chromewebstore.google.com/detail/pmfhomnifobhdenknahmdaekhfjnhfkf/preview?hl=en-GB&authuser=0",
+  extensionUrl: "https://chromewebstore.google.com/detail/uscis-case-monitor-local/pmfhomnifobhdenknahmdaekhfjnhfkf?authuser=0&hl=en-GB",
   donationUrl: "https://www.buymeacoffee.com/",
   contactEmail: "nitsin.ca@gmail.com",
   productName: "Case Clarity"
